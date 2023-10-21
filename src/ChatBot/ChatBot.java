2068fd29e1e590f5c75b7ad67847c69d77ae85a2
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-10-21 14:32:32 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Steve.");
+        System.out.println("I was created in 2023.");
+    }
+}
