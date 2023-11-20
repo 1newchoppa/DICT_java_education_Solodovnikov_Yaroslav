d0b7322e87dbf086c02826df7db48f8b9071b9e9
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-11-20 20:01:48 +0200

    CoffeMachine 3-rd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,19 +1,40 @@
 package CoffeeMachine;
+import java.util.Scanner;
 
 public class CoffeeMachine {
 
     public void makeCoffee() {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterAvailable = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkAvailable = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeansAvailable = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsNeeded = scanner.nextInt();
+
+        int producedCups = Math.min(Math.min(waterAvailable / 200,milkAvailable / 50), coffeeBeansAvailable / 15);
+
+        if (producedCups == cupsNeeded){
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+
+        else if (producedCups > cupsNeeded){
+            System.out.println("Yes, I can make that amount of coffee (and even " + (producedCups - cupsNeeded) + " more than that)");
+        }
+
+        else {
+            System.out.println("No, I can make only " + producedCups + " cups of coffee");
+        }
+
     }
 
     public static void main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
         coffeeMachine.makeCoffee();
     }
+
 }
