5551f9fb4b3c2ef064a71e4ef5cbefd61db1574e
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-11-27 22:07:09 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,19 +1,44 @@
 package TicTacToe;
+import java.util.Scanner;
 
-public class TicTacToe{
-        public static void main(String[] args) {
-            char[][] board = {
-                    {'X', 'O', 'X'},
-                    {'O', 'X', 'O'},
-                    {'X', 'X', 'O'}
-            };
+public class TicTacToe {
 
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(board[i][j] + " ");
-                }
-                System.out.println();
-            }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] board = {
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'}
+        };
 
+        System.out.print(">");
+        String userInput = scanner.nextLine();
+
+        if (userInput.length() != 9) {
+            System.out.println("Рядок повинен містити 9 символів.");
+            return;
         }
+
+        int index = 0;
+
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                char symbol = userInput.charAt(index);
+
+                if (symbol != 'X' && symbol != '0' && symbol != '_') {
+                    System.out.println("Допустимі символи: X, 0, _");
+                    return;
+                }
+
+                board[i][j] = symbol;
+                System.out.print(board[i][j] + " ");
+                index++;
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
+}
+
