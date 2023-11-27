eaa8e9ae3620162a6696f00a01c6c2cb47c95aa1
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-11-27 21:29:30 +0200

    TicTacToe 1-st stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,19 @@
+package TicTacToe;
+
+public class TicTacToe{
+        public static void main(String[] args) {
+            char[][] board = {
+                    {'X', 'O', 'X'},
+                    {'O', 'X', 'O'},
+                    {'X', 'X', 'O'}
+            };
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
