c9d32834e9e855b62a2bc5f9ed0f1117cc454016
1newchoppa <y.m.solodovnikov@student.khai.edu>
2023-12-02 19:08:19 +0200

    TicTacToe 4-th stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -48,46 +48,50 @@ public class TicTacToe {
         }
         System.out.println("---------");
 
-        if (Math.abs(xCount - oCount) > 1) {
-            System.out.println("Impossible");
-            return;
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            String[] coordinates = scanner.nextLine().split(" ");
+
+            if (coordinates.length != 2) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            int x, y;
+
+            try {
+                x = Integer.parseInt(coordinates[0]);
+                y = Integer.parseInt(coordinates[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            if (x < 1 || x > 3 || y < 1 || y > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+
+            x--;
+            y--;
+
+            if (board[x][y] != '_') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+
+            board[x][y] = 'X';
+            break;
         }
 
-        boolean xWins = checkWin(board, 'X');
-        boolean oWins = checkWin(board, 'O');
-
-        if (xWins && oWins) {
-            System.out.println("Impossible");
-        } else if (xWins) {
-            System.out.println("X wins");
-        } else if (oWins) {
-            System.out.println("O wins");
-        } else if (xCount + oCount == 9) {
-            System.out.println("Draw");
-        } else {
-            System.out.println("Game not finished");
-        }
-    }
-
-    public static boolean checkWin(char[][] board, char symbol) {
+        System.out.println("---------");
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
-                return true;
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
             }
-
-            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
-                return true;
-            }
+            System.out.println("|");
         }
-
-        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
-            return true;
-        }
-
-        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
-            return true;
-        }
-
-        return false;
+        System.out.println("---------");
     }
 }
